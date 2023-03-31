0af9addb9014e9e3163569a74fa0bdb0887b6e2d
Ivan <Ivan@IVAN-PC>
2023-03-31 21:18:13 +0200

    Dodan i konfiguriran identity paket

--- DemoWebShop/DemoWebShop/Program.cs
+++ DemoWebShop/DemoWebShop/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DemoWebShop.Areas.Identity.Data;
+using DemoWebShop.Data;
 namespace DemoWebShop
 {
     public class Program
@@ -5,9 +9,18 @@ namespace DemoWebShop
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+                        var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+                                    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                                                builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
@@ -23,12 +36,15 @@ namespace DemoWebShop
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
 
             app.Run();
         }
